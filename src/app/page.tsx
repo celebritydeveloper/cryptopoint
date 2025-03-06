f670ac356a37954752554ce6c383f9519cb6c131
Saviour Essien <31137610+celebritydeveloper@users.noreply.github.com>
2025-03-06 19:16:52 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,14 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   
-  const initData = useLaunchParams()?.initDataRaw || "No initData";
-  const [user, setUser] = useState<any>(parseInitData(initData));
-  console.log(parseInitData(initData));
+  const initData = parseInitData(useLaunchParams()?.initDataRaw) || "No initData";
+  console.log(initData);
+  //const [user, setUser] = useState<any>(parseInitData(initData));
   // const data = WebApp.initDataUnsafe.user; 
   // const userId = initData.id.toString()
 
   useEffect(() => {
     console.log(initData);
-    console.log(parseInitData(initData));
-    setUser((prev: any) => ({...prev, ...parseInitData(initData)}));
-    console.log(user);
     
   }, []);
 
@@ -26,14 +23,14 @@ export default function Home() {
       {/* <Link href={"ton-connect"}>connect </Link> */}
 
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl font-bold cursor-pointer" onClick={()=> {navigator.clipboard.writeText(initData); console.log(initData);
+        <h1 className="text-3xl font-bold cursor-pointer" onClick={()=> {console.log(initData);
         }}>Welco</h1>
 
         {/* <p>{initData?.user?.first_name}</p>
         <p>{initData?.last_name}</p>
         <p>{initData?.photo_url}</p> */}
 
-        <p>{user}</p>
+        {/* <p>{user}</p> */}
 
         <button
           onClick={() => {
